0244a8498015c1c2998418669a1f0460984b14c8
tiagoantunes13 <tiagoantunes9991@gmail.com>
2022-07-20 22:28:16 +0100

    Create, Update and Show actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,39 @@
 class ArticlesController < ApplicationController
-  def show()
+  def show
     @article = Article.find(params[:id])
   end
 
-  def index()
+  def index
     @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:tittle, :description))
+    if @article.save
+      flash[:notice] = "Article successfully created"
+      redirect_to article_path(@article)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:tittle, :description))
+      flash[:notice] = "Article successfully updated"
+      redirect_to article_path(@article)
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 end
