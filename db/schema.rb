fddca418e4bfa58e72690022931c763e9966ae35
tiagoantunes13 <tiagoantunes9991@gmail.com>
2022-07-21 15:54:37 +0100

    Style 2

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_20_013053) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_21_145036) do
   create_table "articles", force: :cascade do |t|
     t.string "tittle"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
