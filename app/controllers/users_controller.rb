7183ab3ebddbc3f63860ebf4a65dff3a38d5e2ac
tiagoantunes13 <tiagoantunes9991@gmail.com>
2022-07-23 20:48:51 +0100

    Update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,9 +22,9 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account has been updated"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
-      render :edit, status: :unprocessable_entity
+      render :show, status: :unprocessable_entity
     end
   end
 
