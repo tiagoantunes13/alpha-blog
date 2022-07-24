58514ac4af46d111699a553026a62ab278b83e0f
tiagoantunes13 <tiagoantunes9991@gmail.com>
2022-07-24 17:41:20 +0100

    Details

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -59,7 +59,7 @@ class ArticlesController < ApplicationController
   end
 
   def require_same_user
-    if current_user != @article.user
+    if current_user != @article.user && !current_user.admin?
       flash[:alert] = "You can only edit or delete your article"
       redirect_to articles_path
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,12 @@ class UsersController < ApplicationController
   before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def new
-    @user = User.new
+    if session[:user_id] != nil
+      redirect_to articles_path
+    else
+      @user = User.new
+    end
+
   end
 
   def create
@@ -43,7 +48,7 @@ class UsersController < ApplicationController
 
   def destroy
     @user.destroy
-    session[:user_id] = nil
+    session[:user_id] = nil if current_user == @user
     flash[:notice] = "Account deleted"
     redirect_to signup_path, status: :see_other#, status: :unprocessable_entity
   end
@@ -56,7 +61,7 @@ class UsersController < ApplicationController
   end
 
   def require_same_user
-    if current_user != @user
+    if current_user != @user && !current_user.admin?
       flash[:alert] = "You can only edit or delete your profile"
       redirect_to root_path
     end
